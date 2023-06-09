bf95c4e6454ecdc7352e8dd576bb7ec25a913481
jhhur <jhhur716@gmail.com>
2023-06-09 17:46:55 +0900

    test: 심판 API 테스트 추가 및 API 추가

--- __mocks__/handlers/handlers.ts
+++ __mocks__/handlers/handlers.ts
@@ -2,5 +2,6 @@ import { atBatHandler } from './atBatHandler';
 import { gameHandler } from './gameHandler';
 import { playerHandler } from './playerHandler';
 import { teamHandler } from './teamHandler';
+import { umpireHandler } from './umpireHandler';
 
-export const handlers = [...atBatHandler, ...gameHandler, ...playerHandler, ...teamHandler];
+export const handlers = [...atBatHandler, ...gameHandler, ...playerHandler, ...teamHandler, ...umpireHandler];
