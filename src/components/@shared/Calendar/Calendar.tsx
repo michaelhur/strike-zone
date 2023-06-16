4293e35176bc07ebe556768c9a75d040524100cf
jhhur <jhhur716@gmail.com>
2023-06-16 11:55:33 +0900

    feat: Calendar 컴포넌트 추가

--- src/components/@shared/Calendar/Calendar.tsx
+++ src/components/@shared/Calendar/Calendar.tsx
@@ -1,19 +1,14 @@
 import 'react-day-picker/dist/style.css';
 
-import { useState } from 'react';
-import { DaySelectionMode, Matcher } from 'react-day-picker';
+import { DayPickerProps } from 'react-day-picker';
 
 import { StyledDayPicker } from '@components/@shared/Calendar/Calendar.styles';
 
-export interface CalendarProps {
-    mode: DaySelectionMode;
-    selected: Matcher;
-}
+export type CalendarProps = DayPickerProps;
 
-export const Calendar = () => {
-    const [selected, setSelected] = useState<Date>();
+export const Calendar = (props: CalendarProps) => {
     const yesterday = new Date();
     yesterday.setDate(yesterday.getDate() - 1);
 
-    return <StyledDayPicker mode="single" selected={selected} onSelect={setSelected} toDate={yesterday} />;
+    return <StyledDayPicker {...props} toDate={yesterday} />;
 };
--- src/pages/Fixture/Fixture.tsx
+++ src/pages/Fixture/Fixture.tsx
@@ -1,11 +1,16 @@
+import { useState } from 'react';
+
 import { Calendar } from '@components/@shared/Calendar/Calendar';
 
 import { FixturePageContainer } from '@pages/Fixture/Fixture.styles';
 
 const Fixture = () => {
+    const [selected, onSelect] = useState<Date>();
+
+    console.log('selected:', selected);
     return (
         <FixturePageContainer>
-            <Calendar />
+            <Calendar mode={'single'} selected={selected} onSelect={onSelect} />
         </FixturePageContainer>
     );
 };
