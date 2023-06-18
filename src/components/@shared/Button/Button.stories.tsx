418c1816e5dfcbae18ddae7dd2ec0546b1ed7e2a
Michael Hur <jhhur716@gmail.com>
2023-06-18 21:57:24 +0900

    feat: Button 컴포넌트 스토리북 추가

--- src/components/@shared/Button/Button.stories.tsx
+++ src/components/@shared/Button/Button.stories.tsx
@@ -1,6 +1,7 @@
 import { Meta, Story } from '@storybook/react';
 
 import { Button, ButtonProps } from '@components/@shared/Button/Button';
+import { ArrowLeftIcon, ArrowRightIcon } from '@components/@shared/Icon';
 
 export default {
     title: 'Components/@shared/Button',
@@ -8,8 +9,21 @@ export default {
 } as Meta;
 
 const Template: Story<ButtonProps> = (args) => {
-    return <Button {...args}>{args.children}</Button>;
+    return (
+        <Button {...args} size={'XSmall'} buttonTheme={'disabled'} buttonType={'button'}>
+            {args.children}
+        </Button>
+    );
 };
 
 export const ButtonTemplate = Template.bind({});
 ButtonTemplate.args = { children: '버튼' };
+
+export const ButtonWithLeftIconTemplate = Template.bind({});
+ButtonWithLeftIconTemplate.args = { children: '버튼', leftIcon: <ArrowLeftIcon /> };
+
+export const ButtonWithRightIconTemplate = Template.bind({});
+ButtonWithRightIconTemplate.args = { children: '버튼', leftIcon: <ArrowRightIcon /> };
+
+export const ButtonWithBothIconTemplate = Template.bind({});
+ButtonWithBothIconTemplate.args = { children: '버튼', leftIcon: <ArrowLeftIcon />, rightIcon: <ArrowRightIcon /> };
