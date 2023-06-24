bed106ce6c35e982a827b152117cc9fd5b6903dc
Michael Hur <jhhur716@gmail.com>
2023-06-24 21:26:36 +0900

    refactor: PlayerGameList 컴포넌트 분리

--- src/pages/PlayerDetailPage/PlayerDetailPage.tsx
+++ src/pages/PlayerDetailPage/PlayerDetailPage.tsx
@@ -1,12 +1,6 @@
 import React from 'react';
 import { useParams } from 'react-router-dom';
 
-import { GameList } from '@components/GameListSection/components/GameList/GameList';
-import { SectionTitleWrapper } from '@components/Layout/Layout';
-import { Loading } from '@components/Loading/Loading';
-
-import { useGetGameByPlayerSlug } from '@hooks/@query/game/useGetGameByPlayerSlug';
-
 import { LeftSection, PlayerDetailPageContainer, RightSection } from '@pages/PlayerDetailPage/PlayerDetailPage.styles';
 import { PlayerGameList } from '@pages/PlayerDetailPage/components/PlayerGameList/PlayerGameList';
 import { PlayerProfileCard } from '@pages/PlayerDetailPage/components/PlayerProfileCard/PlayerProfileCard';
