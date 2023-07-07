25a546391fab3c0ec2fb6fd773cd18859cbbf3f4
jhhur <jhhur716@gmail.com>
2023-07-07 16:38:51 +0900

    feat: 심판관련 Mock API 추가

--- __mocks__/__test__/umpire.test.ts
+++ __mocks__/__test__/umpire.test.ts
@@ -2,6 +2,8 @@ import axios from 'axios';
 import { setupServer } from 'msw/node';
 import { describe, expect } from 'vitest';
 
+import { atBatList } from '../data/atBat';
+import { gameList } from '../data/game';
 import { umpireList } from '../data/umpire';
 import { umpireHandler } from '../handlers/umpireHandler';
 
@@ -39,5 +41,64 @@ describe('심판 API', () => {
 
         expect(response.status).toBe(200);
         expect(data).toEqual(targetUmpire);
+    });
+
+    it('GET /api/umpires/:umpireId/games 요청은 특정 심판의 경기 정보를 리턴한다', async () => {
+        const umpireId = 484499;
+        const response = await axios.get(`/api/umpires/${umpireId}/games`);
+        const data = response.data;
+
+        const targetGameList = gameList
+            .filter((game) => game.umpire && game.umpire.id === umpireId)
+            .sort((a, b) => b.date.localeCompare(a.date));
+
+        expect(response.status).toBe(200);
+        expect(data).toEqual(targetGameList);
+    });
+
+    it('GET /api/umpires/:umpireId/games/latest 요청은 특정 심판의 최신 경기 정보를 리턴한다', async () => {
+        const umpireId = 484499;
+        const response = await axios.get(`/api/umpires/${umpireId}/games/latest`);
+        const data = response.data;
+
+        const targetGameList = gameList
+            .filter((game) => game.umpire && game.umpire.id === umpireId)
+            .sort((a, b) => b.date.localeCompare(a.date))
+            .slice(0, 5);
+
+        expect(response.status).toBe(200);
+        expect(data).toEqual(targetGameList);
+    });
+
+    it('GET /api/umpires/:umpireId/atbats 요청은 특정 심판의 타석 경기 정보를 리턴한다', async () => {
+        const umpireId = 484499;
+        const response = await axios.get(`/api/umpires/${umpireId}/atbats`);
+        const data = response.data;
+
+        const targetAtbatList = atBatList
+            .filter((atbat) => atbat.umpire!.id === Number(umpireId))
+            .sort((a, b) => b.date.localeCompare(a.date) || a.atBatIndex - b.atBatIndex);
+
+        expect(response.status).toBe(200);
+        expect(data).toEqual(targetAtbatList);
+    });
+
+    it('GET /api/umpires/:umpireId/atbats/latest 요청은 특정 심판의 타석 경기 정보를 리턴한다', async () => {
+        const umpireId = 484499;
+        const response = await axios.get(`/api/umpires/${umpireId}/atbats/latest`);
+        const data = response.data;
+
+        const targetGameList = gameList
+            .filter((game) => game.umpire && game.umpire.id === Number(umpireId))
+            .sort((a, b) => b.date.localeCompare(a.date))
+            .slice(0, 5)
+            .map((game) => game.id);
+
+        const targetAtbatList = atBatList
+            .filter((atbat) => targetGameList.includes(atbat.game!.id))
+            .sort((a, b) => b.date.localeCompare(a.date) || a.atBatIndex - b.atBatIndex);
+
+        if (!targetAtbatList) expect(response.status).toBe(200);
+        expect(data).toEqual(targetAtbatList);
     });
 });
--- __mocks__/handlers/umpireHandler.ts
+++ __mocks__/handlers/umpireHandler.ts
@@ -1,4 +1,9 @@
+import { atBatList } from '@mocks/data/atBat';
+import { gameList } from '@mocks/data/game';
 import { rest } from 'msw';
+
+import { AtBat } from '@typings/atbat';
+import { Game } from '@typings/game';
 
 import { Umpire } from '../../src/typings/umpire';
 import { umpireList } from '../data/umpire';
@@ -20,5 +25,60 @@ export const umpireHandler = [
         if (!targetUmpire) return res(ctx.status(400), ctx.json({ message: '해당 심판은 존재하지 않습니다.' }));
 
         return res(ctx.status(200), ctx.json(targetUmpire));
+    }),
+
+    rest.get<Game[]>('/api/umpires/:umpireId/games', async (req, res, ctx) => {
+        const { umpireId } = req.params;
+        const targetGameList = gameList
+            .filter((game) => game.umpire && game.umpire.id === Number(umpireId))
+            .sort((a, b) => b.date.localeCompare(a.date));
+
+        if (!targetGameList)
+            return res(ctx.status(400), ctx.json({ message: '해당 심판의 경기 정보가 존재하지 않습니다.' }));
+
+        return res(ctx.status(200), ctx.json(targetGameList));
+    }),
+
+    rest.get<Game[]>('/api/umpires/:umpireId/games/latest', async (req, res, ctx) => {
+        const { umpireId } = req.params;
+        const targetGameList = gameList
+            .filter((game) => game.umpire && game.umpire.id === Number(umpireId))
+            .sort((a, b) => b.date.localeCompare(a.date))
+            .slice(0, 5);
+
+        if (!targetGameList)
+            return res(ctx.status(400), ctx.json({ message: '해당 심판의 경기 정보가 존재하지 않습니다.' }));
+
+        return res(ctx.status(200), ctx.json(targetGameList));
+    }),
+
+    rest.get<AtBat[]>('/api/umpires/:umpireId/atbats', async (req, res, ctx) => {
+        const { umpireId } = req.params;
+        const targetAtbatList = atBatList
+            .filter((atbat) => atbat.umpire!.id === Number(umpireId))
+            .sort((a, b) => b.date.localeCompare(a.date) || a.atBatIndex - b.atBatIndex);
+
+        if (!targetAtbatList)
+            return res(ctx.status(400), ctx.json({ message: '해당 심판의 타석 정보가 존재하지 않습니다.' }));
+
+        return res(ctx.status(200), ctx.json(targetAtbatList));
+    }),
+
+    rest.get<AtBat[]>('/api/umpires/:umpireId/atbats/latest', async (req, res, ctx) => {
+        const { umpireId } = req.params;
+        const targetGameList = gameList
+            .filter((game) => game.umpire && game.umpire.id === Number(umpireId))
+            .sort((a, b) => b.date.localeCompare(a.date))
+            .slice(0, 5)
+            .map((game) => game.id);
+
+        const targetAtbatList = atBatList
+            .filter((atbat) => targetGameList.includes(atbat.game!.id))
+            .sort((a, b) => b.date.localeCompare(a.date) || a.atBatIndex - b.atBatIndex);
+
+        if (!targetAtbatList)
+            return res(ctx.status(400), ctx.json({ message: '해당 심판의 타석 정보가 존재하지 않습니다.' }));
+
+        return res(ctx.status(200), ctx.json(targetAtbatList));
     }),
 ];
