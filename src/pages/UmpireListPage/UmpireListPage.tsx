426710a343b4574fa634679018b2d0250759a861
jhhur <jhhur716@gmail.com>
2023-07-12 17:53:23 +0900

    style: 심판 리스트 웹 반응형 구현

--- src/pages/UmpireListPage/UmpireListPage.tsx
+++ src/pages/UmpireListPage/UmpireListPage.tsx
@@ -1,11 +1,17 @@
+import { useRecoilValue } from 'recoil';
+
 import { SectionTitleWrapper } from '@components/Layout/Layout';
 
 import { UmpireListPageContainer } from '@pages/UmpireListPage/UmpireListPage.styles';
 import { UmpireList } from '@pages/UmpireListPage/components/UmpireList/UmpireList';
 
+import { sidebarCollapseState } from '@recoils/sidebar/atom';
+
 const UmpireListPage = () => {
+    const isSidebarOpen = useRecoilValue(sidebarCollapseState);
+
     return (
-        <UmpireListPageContainer>
+        <UmpireListPageContainer isSidebarOpen={isSidebarOpen}>
             <SectionTitleWrapper>
                 <h2>Umpire</h2>
             </SectionTitleWrapper>
